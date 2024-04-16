8fd148af2d64192664ff71e101b143043953554b
ahasan2912 <habib-15-4284@diu.edu.bd>
2024-04-16 23:03:01 +0600

    added

--- src/main.js
+++ src/main.js
@@ -2,36 +2,36 @@ let shop = document.getElementById('shop');
 
 let shopItemsData = [
     {
-    id: "jfhgbvnscs1",
+    id: "one",
     name: "Casual Shirt",
     price: 45,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "image/img-1.jpg", 
 },
 {
-    id: "jfhgbvnscs2",
+    id: "two",
     name: "Office Shirt",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "image/img-2.jpg",
 },
 {
-    id: "jfhgbvnscs3",
+    id: "three",
     name: "T Shirt",
     price: 25,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "image/img-3.jpg",
 },
 {
-    id: "jfhgbvnscs4",
+    id: "four",
     name: "Mens Suit",
     price: 300,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "image/img-4.jpg",
 }
 ]
-
-
+// !Store the Data inside basket when the click event.
+let basket = [];
 
 let generateShop =() => {
     return (shop.innerHTML = shopItemsData.map((x)=>{
@@ -45,9 +45,9 @@ let generateShop =() => {
           <div class="price-quantity">
             <h2>$ ${price}</h2>
             <div class="buttons">
-              <i class="bi bi-dash-lg"></i>
-              <div class="quantity">0</div>
-              <i class="bi bi-plus-lg"></i>
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">0</div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
             </div>
           </div>
         </div>
@@ -55,4 +55,45 @@ let generateShop =() => {
     }).join(''));
 };
 
-generateShop();
+generateShop(); 
+
+let increment = (id)=>{
+  let selectedItem = id;
+  let search = basket.find((x)=> x.id === selectedItem.id);
+
+  if(search === undefined){
+    basket.push({
+      id: selectedItem.id,
+      item:1,
+    });
+  }
+  else{
+    search.item+= 1;
+  }
+  
+  //console.log(basket);
+  update(selectedItem.id);
+}; 
+let decrement = (id)=>{ 
+  let selectedItem = id;
+  let search = basket.find((x)=> x.id === selectedItem.id);
+
+  if(search.item === 0) return ;
+  else{
+    search.item-= 1;
+  }
+  
+  //console.log(basket);
+  update(selectedItem.id);
+};
+let update = (id)=>{
+  let search = basket.find((x) => x.id === id);
+  //console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation()
+}; 
+
+let calculation = () =>{
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y)=>x+y,0);
+}
